80922c41bedd52ba1f065effda5f3d27b72efcb8
Hamie <h.nouri@sae.edu>
2024-06-15 21:30:32 +0100

    READ ME File added in Script folder for Future Programmer

---
+++ Assets/Scripts/ReadME_Future_Prog.cs
@@ -0,0 +1,36 @@
+/*
+Dear Future Programmer, 
+Hope you are well,
+
+I am leaving this file to help you with picking up where I left off:
+
+tbh I haven't done much...
+
+I've made a Scene called Mechanics for prototyping.
+
+The "Player" in the hierarchy is there to hold the position of the player and the related objects. [The Green One]
+	PlayerBee is where all the scripts related to the player attached, specifically the Movement script that relies on the character controller.[The Blue One]
+
+For the Camera/View rotation, the script is attached to the Camera. 
+
+The rest are expendable and feel free to get rid of them, most of them were to see how the animation, texturing and such work. 
+
+Partical system is just stretch goal for now. 
+_______________________
+
+=====>>  What you need to do from now on...  <<=====
+
+Tommaso has a Backlog, feel free to continue from 3rd task onwards
+
+The most important is the collision between the bee/player and the flower to activate the flower and its pollens animation. 
+Its a simple collision script with anim. 
+
+And what comes after is dependant on Tommaso and the teams direction. 
+_______________________
+If you have any questions, I am still reachable in the Discord Server but I have the Server on Mute so @ me if you need me 
+
+Good Luck
+hope you make a bee-you-tea-ful game!
+
+Have Fun ^_^)/
+*/
